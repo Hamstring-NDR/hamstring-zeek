36fc1a28b9b4e6a8051e4cd23678482acce85564
maldwg <maxludwig3@t-online.de>
2026-07-21 12:12:38 +0200

    add support for MAC to fix tests

--- tests/test_command_executor.cpp
+++ tests/test_command_executor.cpp
@@ -3,6 +3,19 @@
 #include <csignal>
 #include <gtest/gtest.h>
 
+namespace {
+
+class SignalMaskRestorer {
+  public:
+    explicit SignalMaskRestorer(const sigset_t &mask) : mask_(mask) {}
+    ~SignalMaskRestorer() { pthread_sigmask(SIG_SETMASK, &mask_, nullptr); }
+
+  private:
+    sigset_t mask_;
+};
+
+} // namespace
+
 TEST(PosixCommandExecutorTest, UnblocksShutdownSignalsBeforeExec) {
     sigset_t shutdown_signals;
     sigemptyset(&shutdown_signals);
@@ -11,11 +24,8 @@ TEST(PosixCommandExecutorTest, UnblocksShutdownSignalsBeforeExec) {
 
     sigset_t original_mask;
     ASSERT_EQ(pthread_sigmask(SIG_BLOCK, &shutdown_signals, &original_mask), 0);
+    SignalMaskRestorer restorer(original_mask);
 
     PosixCommandExecutor executor;
-    const int            result =
-        executor.execute({"sh", "-c", "grep -q '^SigBlk:[[:space:]]*0000000000000000$' /proc/self/status"});
-
-    ASSERT_EQ(pthread_sigmask(SIG_SETMASK, &original_mask, nullptr), 0);
-    EXPECT_EQ(result, 0);
+    EXPECT_EQ(executor.execute({COMMAND_EXECUTOR_SIGNAL_HELPER}), 0);
 }
